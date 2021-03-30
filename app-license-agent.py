ed7628d29e9d552c3fceccf0b4b535fcfd97b14e
Carlos Doki <carlosdoki@gmail.com>
2021-03-30 18:24:13 -0300

    Correcao para Pyhton3

--- app-license-agent.py
+++ app-license-agent.py
@@ -15,24 +15,31 @@ intervalo = ''
 token = ''
 cookies = ''
 
+
 def get_auth(host, port, user, password, account):
     url = '{}:{}/controller/auth'.format(host, port)
+    data_string = user + "@" + account + ":" + password
+    data_bytes = data_string.encode("utf-8")
+    headerBasic = base64.b64encode(data_bytes).decode('utf-8')
     headers = {
-        'Authorization': 'Basic ' + base64.b64encode(user + "@" + account + ":" + password)  
+        'Authorization': 'Basic ' + headerBasic
     }
+
     params = (
         ('action', 'login'),
     )
     response = requests.get(url, headers=headers, params=params)
     global token
     global cookies
-    cookies = response.cookies 
+    cookies = response.cookies
     token = response.cookies.get("X-CSRF-TOKEN")
 
     return 0
 
+
 def licencas():
-    url = '{}:{}/controller/restui/licenseRule/getAllLicenseModuleProperties'.format(host, port)
+    url = '{}:{}/controller/restui/licenseRule/getAllLicenseModuleProperties'.format(
+        host, port)
     data = '''{
         "type": "BEFORE_NOW",
         "durationInMinutes": ''' + intervalo + ''',
@@ -41,59 +48,72 @@ def licencas():
         "timeRange": null,
         "timeRangeAdjusted": false }
         '''
+    data_string = user + "@" + account + ":" + password
+    data_bytes = data_string.encode("utf-8")
+    headerBasic = base64.b64encode(data_bytes).decode('utf-8')
     headers = {
-        'Authorization': 'Basic ' + base64.b64encode(user + "@" + account + ":" + password),
-        'X-CSRF-TOKEN' : token,
+        'Authorization': 'Basic ' + headerBasic,
+        'X-CSRF-TOKEN': token,
         'Content-Type': 'application/json',
-        'Accept' : 'application/json, text/plain, */*'
+        'Accept': 'application/json, text/plain, */*'
 
     }
     r = requests.post(url, data=data, headers=headers, cookies=cookies)
     if r.status_code == 200:
-        file = open('{}.csv'.format("licencas"),'w')
-        texto='{};{}'.format("Tipo","Quantidade nos Ultimos " + intervalo + " mins")
+        file = open('{}.csv'.format("licencas"), 'w')
+        texto = '{};{}'.format(
+            "Tipo", "Quantidade nos Ultimos " + intervalo + " mins")
         file.write(texto + '\n')
-        data=r.json()
-        data=json.dumps(data)
-        data=json.loads(data)
+        data = r.json()
+        data = json.dumps(data)
+        data = json.loads(data)
         for tipo in data:
-            data2=json.dumps(data[tipo])
-            data2=json.loads(data2)
+            data2 = json.dumps(data[tipo])
+            data2 = json.loads(data2)
             if data2 is not None:
-                texto='{};{}'.format(tipo, data2["peakUsage"])
+                texto = '{};{}'.format(tipo, data2["peakUsage"])
                 file.write(texto + '\n')
-                #print(data2)
+                # print(data2)
         file.close
     return 0
+
+
 def agentes():
-    url = '{}:{}/controller/restui/agent/setting/getAppServerAgents'.format(host, port)
+    url = '{}:{}/controller/restui/agent/setting/getAppServerAgents'.format(
+        host, port)
+    data_string = user + "@" + account + ":" + password
+    data_bytes = data_string.encode("utf-8")
+    headerBasic = base64.b64encode(data_bytes).decode('utf-8')
     headers = {
-        'Authorization': 'Basic ' + base64.b64encode(user + "@" + account + ":" + password),
-        'X-CSRF-TOKEN' : token,
+        'Authorization': 'Basic ' + headerBasic,
+        'X-CSRF-TOKEN': token,
         'Content-Type': 'application/json',
-        'Accept' : 'application/json, text/plain, */*'
+        'Accept': 'application/json, text/plain, */*'
 
     }
     params = {'output': 'json'}
     r = requests.get(url, params=params, headers=headers, cookies=cookies)
     if r.status_code == 200:
-        file = open('{}.csv'.format("agents"),'w')
-        texto='{};{};{};{}'.format("Aplicacao","NodeName","hostname","Tipo", "Versao")
+        file = open('{}.csv'.format("agents"), 'w')
+        texto = '{};{};{};{}'.format(
+            "Aplicacao", "NodeName", "hostname", "Tipo", "Versao")
         file.write(texto + '\n')
         for resposta in r.json():
-            #print(resposta)
-            #print()
-            #print(resposta["hostName"])
-            #print(resposta["applicationComponentNodeName"])
-            #print(resposta["agentDetails"])
-            agentDetails=json.dumps(resposta["agentDetails"])
-            agentDetails=json.loads(agentDetails)
-            #print(agentDetails["type"])
-            #print(agentDetails["agentVersion"])
-            texto='{};{};{};{};{}'.format(resposta["applicationName"],resposta["applicationComponentNodeName"], resposta["hostName"], agentDetails["type"],agentDetails["agentVersion"])
+            # print(resposta)
+            # print()
+            # print(resposta["hostName"])
+            # print(resposta["applicationComponentNodeName"])
+            # print(resposta["agentDetails"])
+            agentDetails = json.dumps(resposta["agentDetails"])
+            agentDetails = json.loads(agentDetails)
+            # print(agentDetails["type"])
+            # print(agentDetails["agentVersion"])
+            texto = '{};{};{};{};{}'.format(resposta["applicationName"], resposta["applicationComponentNodeName"],
+                                            resposta["hostName"], agentDetails["type"], agentDetails["agentVersion"])
             file.write(texto + '\n')
-        file.close    
+        file.close
     return 0
+
 
 def process():
     get_auth(host, port, user, password, account)
@@ -102,6 +122,7 @@ def process():
 
     return 0
 
+
 def main():
     global host
     global port
@@ -109,24 +130,24 @@ def main():
     global password
     global account
     global intervalo
-    global cabecalho 
+    global cabecalho
 
-    if len(sys.argv) == 7 :
-        host = sys.argv[1] 
+    if len(sys.argv) == 7:
+        host = sys.argv[1]
         port = sys.argv[2]
         user = sys.argv[3]
         password = sys.argv[4]
         account = sys.argv[5]
         intervalo = sys.argv[6]
 
-  
         cabecalho = '{};{}'.format(datetime.datetime.now(), intervalo)
         print(datetime.datetime.now())
         process()
         print(datetime.datetime.now())
     else:
-        print 'app-license-agent.py <host> <port> <user> <password> <account> <intervalo>'
+        print('app-license-agent.py <host> <port> <user> <password> <account> <intervalo>')
         sys.exit(2)
+
 
 if __name__ == '__main__':
     main()
